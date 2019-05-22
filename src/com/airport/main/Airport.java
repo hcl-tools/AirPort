5455b7bfc5b31adf1b82c71b117a514b0daf5648
crazywolf132 <crazywolf132@gmail.com>
2019-05-22 13:13:48 +1000

    AirportHandler

--- src/com/airport/main/Airport.java
+++ src/com/airport/main/Airport.java
@@ -1,15 +1,25 @@
 package com.airport.main;
 
+import com.airport.bean.Passengers;
+import com.airport.bean.Planes;
+import com.airport.bean.Staff;
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Airport {
+
+    private static Map<String, Planes> planesMap = new HashMap<>();
+    private static Map<String, Staff> staffMap = new HashMap<>();
+    private static Map<String, Passengers> passengersMap = new HashMap<>();
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
     }
 
     public void mainMenu(Scanner keyboard) {
-        
+
     }
 
     public void planeMenu(Scanner keyboard) {
