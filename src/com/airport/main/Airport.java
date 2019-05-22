2b2f041ee59cc79d8d86a3791971c3dbbf5acb23
crazywolf132 <crazywolf132@gmail.com>
2019-05-22 13:13:37 +1000

    Airport menu setup

--- src/com/airport/main/Airport.java
+++ src/com/airport/main/Airport.java
@@ -1,8 +1,26 @@
 package com.airport.main;
 
+import java.util.Scanner;
+
 public class Airport {
 
     public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+    }
+
+    public void mainMenu(Scanner keyboard) {
         
+    }
+
+    public void planeMenu(Scanner keyboard) {
+
+    }
+
+    public void staffMenu(Scanner keyboard) {
+
+    }
+
+    public void passengerMenu(Scanner keyboard) {
+
     }
 }
